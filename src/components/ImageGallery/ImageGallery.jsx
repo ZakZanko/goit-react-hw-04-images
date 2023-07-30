79c58d1def7194ab802ae1e645d7545235d5987f
ZakZanko <zakzanko2112@gmail.com>
2023-07-30 17:34:53 +0300

    fixed

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Button from '../Button/Button';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
